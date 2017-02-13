f48d5dcb3ecb6ca9d753d171ab26c61ea6ef61ff
dev@addmira.com <>
2017-02-13 08:12:38 +0100

    Configuración inicial de Laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 - Addmira</div>
+                <div class="title">Laravel 5 - Addmira360</div>
             </div>
         </div>
     </body>
